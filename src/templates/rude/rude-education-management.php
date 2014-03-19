89d5c849c0a27e150c625046bcc01f72ae34aca8
SapozhnikovAN <anton.sapognikov@gmail.com>
2014-03-19 22:55:30 +0300

    time budget table template added

--- src/templates/rude/rude-education-management.php
+++ src/templates/rude/rude-education-management.php
@@ -1,6 +1,7 @@
 <? namespace rude; ?>
 
 <? require_once 'ajax/rude-ajax-education.php'; ?>
+<? require_once 'rude-table-time-budget.php'; ?>
 
 <div class="box">
 	<h2>Управление учебной программой</h2>
@@ -8,6 +9,6 @@
 
 <div class="middle">
 	<div class="middle-item">
-
+		<? table_time_budget::html() ?>
 	</div>
 </div>
