c146d59b52bb39c5170d5b4d31e4f88169cc84f7
archon033 <archon03@gmail.com>
2014-04-03 09:13:00 +0300

    css fix + added calendar date

--- src/templates/rude/rude-table-time-budget.php
+++ src/templates/rude/rude-table-time-budget.php
@@ -121,7 +121,7 @@ class table_time_budget {
             <td class="no-border-bottom">8</td>
             <td class="no-border-bottom">15</td>
             <td class="no-border-bottom">22</td>
-            <td class="no-border-bottom">
+            <td class="no-border-bottom no-border-top">
                 <div class="underline">29</div>09
             </td>
             <td class="no-border-bottom">6</td>
@@ -189,76 +189,76 @@ class table_time_budget {
             <td class="no-border-bottom">24</td>
         </tr>
         <tr>
-            <td class="no-border-top">1</td>
-            <td class="no-border-top">8</td>
-            <td class="no-border-top">15</td>
-            <td class="no-border-top">22</td>
+            <td class="no-border-top">7</td>
+            <td class="no-border-top">14</td>
+            <td class="no-border-top">21</td>
+            <td class="no-border-top">28</td>
             <td class="no-border-top">
-                <div class="underline">29</div>09
+                <div class="underline">05</div>10
             </td>
-            <td class="no-border-top">6</td>
-            <td class="no-border-top">13</td>
-            <td class="no-border-top">20</td>
-            <td class="no-border-top">
-                <div class="underline">27</div>10
-            </td>
-            <td class="no-border-top">3</td>
-            <td class="no-border-top">10</td>
-            <td class="no-border-top">17</td>
-            <td class="no-border-top">24</td>
-            <td class="no-border-top">1</td>
-            <td class="no-border-top">8</td>
-            <td class="no-border-top">15</td>
-            <td class="no-border-top">22</td>
-            <td class="no-border-top">
-                <div class="underline">29</div>12
-            </td>
-            <td class="no-border-top">5</td>
             <td class="no-border-top">12</td>
             <td class="no-border-top">19</td>
+            <td class="no-border-top">26</td>
             <td class="no-border-top">
-                <div class="underline">26</div>01
+                <div class="underline">02</div>11
             </td>
-            <td class="no-border-top">2</td>
-            <td class="no-border-top">9</td>
-            <td class="no-border-top">16</td>
-            <td class="no-border-top">
-                <div class="underline">23</div>02
-            </td>
-            <td class="no-border-top">2</td>
-            <td class="no-border-top">9</td>
+            <td class="no-border-top">7</td>
             <td class="no-border-top">16</td>
             <td class="no-border-top">23</td>
+            <td class="no-border-top">30</td>
+            <td class="no-border-top">7</td>
+            <td class="no-border-top">14</td>
+            <td class="no-border-top">21</td>
+            <td class="no-border-top">28</td>
             <td class="no-border-top">
-                <div class="underline">30</div>03
+                <div class="underline">04</div>01
             </td>
-            <td class="no-border-top">6</td>
-            <td class="no-border-top">13</td>
-            <td class="no-border-top">20</td>
-            <td class="no-border-top">
-                <div class="underline">27</div>04
-            </td>
-            <td class="no-border-top">4</td>
             <td class="no-border-top">11</td>
             <td class="no-border-top">18</td>
             <td class="no-border-top">25</td>
-            <td class="no-border-top">1</td>
+            <td class="no-border-top">
+                <div class="underline">01</div>02
+            </td>
             <td class="no-border-top">8</td>
             <td class="no-border-top">15</td>
             <td class="no-border-top">22</td>
             <td class="no-border-top">
-                <div class="underline">29</div>06
+                <div class="underline">01</div>03
             </td>
-            <td class="no-border-top">6</td>
-            <td class="no-border-top">13</td>
-            <td class="no-border-top">20</td>
+            <td class="no-border-top">8</td>
+            <td class="no-border-top">15</td>
+            <td class="no-border-top">22</td>
+            <td class="no-border-top">29</td>
             <td class="no-border-top">
-                <div class="underline">27</div>07
+                <div class="underline">05</div>04
             </td>
-            <td class="no-border-top">3</td>
+            <td class="no-border-top">12</td>
+            <td class="no-border-top">19</td>
+            <td class="no-border-top">26</td>
+            <td class="no-border-top">
+                <div class="underline">03</div>05
+            </td>
             <td class="no-border-top">10</td>
             <td class="no-border-top">17</td>
             <td class="no-border-top">24</td>
+            <td class="no-border-top">31</td>
+            <td class="no-border-top">7</td>
+            <td class="no-border-top">14</td>
+            <td class="no-border-top">21</td>
+            <td class="no-border-top">28</td>
+            <td class="no-border-top">
+                <div class="underline">05</div>07
+            </td>
+            <td class="no-border-top">12</td>
+            <td class="no-border-top">19</td>
+            <td class="no-border-top">26</td>
+            <td class="no-border-top">
+                <div class="underline">02</div>08
+            </td>
+            <td class="no-border-top">9</td>
+            <td class="no-border-top">16</td>
+            <td class="no-border-top">23</td>
+            <td class="no-border-top">31</td>
         </tr>
         </table>
     <?
@@ -275,8 +275,7 @@ class table_time_budget {
                     </td>
 
                     <? for ($j = 0; $j < 52; $j++) : ?>
-                        <td class = "td-width" id = <?echo $j?>>0
-                        </td>
+                        <td class = "td-width" id = <?echo $j?>><?= " "?></td>
                     <? endfor; ?>
                 </tr>
             </table>
