5d0c552e13016bd2d23b0b3b36f19a3db9327e77
wild-stas <wild_stas@mail.ru>
2014-05-22 16:40:03 +0200

    main directions added #2

---
+++ src/rude-directions.php
@@ -0,0 +1,80 @@
+<?
+
+namespace rude;
+
+class directions
+{
+	public static function get($field = false)
+	{
+		$q = new query(RUDE_TABLE_DIRECTIONS);
+
+
+		if ($field === false)
+		{
+			$q->start();
+
+			return $q->get_object_list();
+		}
+
+
+		if (is_int($field))
+		{
+			$q->where(RUDE_FIELD_ID, $field);
+		}
+		else if (is_string($field))
+		{
+			$q->where(RUDE_FIELD_NAME, $field);
+		}
+
+		$q->start();
+
+
+		return $q->get_object();
+	}
+
+	public static function getcode($field = false)
+	{
+		$q = new query(RUDE_TABLE_DIRECTIONS);
+
+
+		if ($field === false)
+		{
+			$q->start();
+
+			return $q->get_object_list();
+		}
+
+
+		if (is_int($field))
+		{
+			$q->where(RUDE_FIELD_ID, $field);
+		}
+		else if (is_string($field))
+		{
+			$q->where(RUDE_FIELD_CODE, $field);
+		}
+
+		$q->start();
+
+
+		return $q->get_object();
+	}
+
+	public static function add($name, $code)
+	{
+		$q = new cquery(RUDE_TABLE_DIRECTIONS);
+		$q->add(RUDE_FIELD_NAME, $name);
+		$q->add(RUDE_FIELD_CODE, $code);
+		$q->start();
+
+		return $q->get_id();
+	}
+
+	public static function delete($name)
+	{
+		$q = new dquery(RUDE_TABLE_DIRECTIONS);
+		$q->where(RUDE_FIELD_NAME, $name);
+		$q->limit(1);
+		$q->start();
+	}
+}
